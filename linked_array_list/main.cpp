fe40e45c4a7c23ca780086f3de72c344116c85ad
sbqsbqsbq <sbqsbqsbq@naver.com>
2021-10-16 15:01:53 +0900

    Feat: comment is added

--- linked_array_list/main.cpp
+++ linked_array_list/main.cpp
@@ -8,7 +8,7 @@ int count = 0;
 void addBack(int data) {
     arr[count] = data;
     count++;
-}
+} // 특정 데이터를 배열 뒤에 담기
 
 void addFirst(int data) {
     for (int i = count; i >= 1; i--) {
@@ -16,14 +16,14 @@ void addFirst(int data) {
     }
     arr[0] = data;
     count++;
-}
+} // 특정 데이터를 배열 맨 앞에 담기
 
 void removeAt(int index) {
     for (int i = index; i < count - 1; i++) {
         arr[i] = arr[i + 1];
     }
     count--;
-}
+} // 특정 데이터를 지우는 함수
 
 void show() {
     for (int i = 0; i < count; i++) {
